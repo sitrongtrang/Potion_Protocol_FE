dbfe4867b9bbc2f3ea3520d3099bd66f1d7701c1
Bunhiacopxki <164136561+Bunhiacopxki@users.noreply.github.com>
2025-07-04 13:54:09 +0700

    Animation Fire

---
+++ Assets/Scripts/Loaders/RandomAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RandomAnimator : MonoBehaviour
+{
+    public AnimatorOverrideController[] OverrideControllers;
+    void Awake()
+    {
+        if (OverrideControllers == null || OverrideControllers.Length == 0)
+        {
+            Debug.LogError("Chưa thiết lập overrideControllers!");
+            return;
+        }
+        Animator Anim = GetComponent<Animator>();
+        if (Anim == null)
+        {
+            Debug.LogError("Prefab thiếu component Animator!");
+            return;
+        }
+        int idx = Random.Range(0, OverrideControllers.Length);
+        Anim.runtimeAnimatorController = OverrideControllers[idx];
+    }
+}
