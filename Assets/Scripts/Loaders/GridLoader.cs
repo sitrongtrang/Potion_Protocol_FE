746a7b37bfad7eadfeedaadfb570eaeecdc0f6d6
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-08 17:05:54 +0700

    add initialize path finder in script

--- Assets/Scripts/Loaders/GridLoader.cs
+++ Assets/Scripts/Loaders/GridLoader.cs
@@ -4,6 +4,8 @@ using UnityEngine.Tilemaps;
 public class GridLoader : MonoBehaviour
 {
     public static GridLoader Instance { get; private set; }
+    private static readonly float CELL_SCALE = 0.5f;
+    private Pathfinding _pathfinding;
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -14,17 +16,50 @@ public class GridLoader : MonoBehaviour
 
         Instance = this;
     }
-    public void LoadGrid(GameObject map)
+    public void LoadGrid(GameObject mapGameobject)
     {
-        Grid grid = map.GetComponent<Grid>();
-        Vector3 tileSize = grid.cellSize;
-        // Vector2 tileSize = tilemap.cellSize;
+        Grid grid = mapGameobject.GetComponent<Grid>();
+        Vector2 tileSize = grid.cellSize;
 
-        // BoundsInt bounds = tilemap.cellBounds;
-        // int tilesX = bounds.size.x;
-        // int tilesY = bounds.size.y;
+        float cellSize = Mathf.Min(tileSize.x, tileSize.y) * CELL_SCALE;
 
-        // Vector3Int bottomLeftCell = tilemap.cellBounds.min; // Bottom-left corner cell in grid space
-        // Vector2 worldPos = tilemap.CellToWorld(bottomLeftCell);
+        Tilemap[] tilemaps = mapGameobject.GetComponentsInChildren<Tilemap>();
+
+        Vector3Int globalMinCell = new(int.MaxValue, int.MaxValue, 0);
+        int maxXLength = 0, maxYLength = 0;
+        foreach (Tilemap tilemap in tilemaps)
+        {
+            BoundsInt bounds = tilemap.cellBounds;
+            Vector3Int min = bounds.min;
+
+            maxXLength = Mathf.Max(maxXLength, bounds.size.x);
+            maxYLength = Mathf.Max(maxYLength, bounds.size.y);
+
+            if (min.x < globalMinCell.x) globalMinCell.x = min.x;
+            if (min.y < globalMinCell.y) globalMinCell.y = min.y;
+        }
+        maxXLength = Mathf.CeilToInt((float)maxXLength / CELL_SCALE);
+        maxYLength = Mathf.CeilToInt((float)maxYLength / CELL_SCALE);
+
+        Vector2 bottomLeftWorldPos = grid.GetCellCenterWorld(globalMinCell);
+
+        GameObject gridmap = new("Grid Map");
+        gridmap.transform.SetParent(mapGameobject.transform);
+        gridmap.transform.localPosition = Vector2.zero;
+        for (int x = 0; x < maxXLength; x++)
+        {
+            for (int y = 0; y < maxYLength; y++)
+            {
+                GameObject obj = new GameObject("Grid Cell");
+                obj.transform.SetParent(gridmap.transform);
+                obj.transform.localPosition = bottomLeftWorldPos + new Vector2(x, y) * cellSize;
+
+                obj.AddComponent<BoxCollider2D>();
+                BoxCollider2D collider = obj.GetComponent<BoxCollider2D>();
+                collider.size = new Vector2(cellSize, cellSize);
+            }
+        }
+
+        _pathfinding = new Pathfinding(maxXLength, maxYLength, cellSize, bottomLeftWorldPos);
     }
 }
