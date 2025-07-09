59a6bd62ebf7ec84cf904232e909de4393e6645e
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-09 16:52:03 +0700

    add spawn code for enemy spawner config

--- Assets/Scripts/ScriptableObjects/Enemy/EnemySpawnerConfig.cs
+++ Assets/Scripts/ScriptableObjects/Enemy/EnemySpawnerConfig.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
-public abstract class EnemySpawnerConfig : ScriptableObject
+[CreateAssetMenu(fileName = "EnemySpawnerConfig", menuName = "Scriptable Objects/Enemy/Spawner/Config")]
+public class EnemySpawnerConfig : ScriptableObject
 {
     [field: SerializeField] public float MinSpawnInterval { get; private set; }
     [field: SerializeField] public float MaxSpawnInterval { get; private set; }
     [field: SerializeField] public EnemyConfig[] EnemyConfigsToSpawn { get; private set; }
-    public abstract void Spawn(Vector2 position, int indexPosition);
+    public void Spawn(EnemySpawner spawner, Vector2 position, int indexPosition)
+    {
+        int index = Random.Range(0, EnemyConfigsToSpawn.Length);
+        GameObject enemy = Instantiate(EnemyConfigsToSpawn[index].Prefab.gameObject, spawner.transform.position, Quaternion.identity);
+        enemy.GetComponent<EnemyController>().Initialize(EnemyConfigsToSpawn[index], spawner, position, indexPosition);
+    }
 }
