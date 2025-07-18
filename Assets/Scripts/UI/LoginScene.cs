2aae90414664a31844e7388c5dd7924ff7b9f8fe
Bunhiacopxki <164136561+Bunhiacopxki@users.noreply.github.com>
2025-07-18 17:04:15 +0700

    login scene and room scene

---
+++ Assets/Scripts/UI/LoginScene.cs
@@ -0,0 +1,44 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginScene : MonoBehaviour
+{
+    [SerializeField] private Button _mainButton;
+    [SerializeField] private InputField _passwordField;
+    [SerializeField] private Button _toggleButton;
+    [SerializeField] private Sprite _eyeClosedSprite;
+    [SerializeField] private Sprite _eyeOpenSprite;           
+
+    private bool _isPasswordVisible = false;
+    private Image _buttonImage;
+
+    void Awake()
+    {
+        _buttonImage = _toggleButton.GetComponent<Image>();
+        _toggleButton.onClick.AddListener(TogglePasswordVisibility);
+    }
+
+    void TogglePasswordVisibility()
+    {
+        _isPasswordVisible = !_isPasswordVisible;
+
+        if (_isPasswordVisible)
+        {
+            _passwordField.contentType = InputField.ContentType.Standard;
+            _buttonImage.sprite = _eyeOpenSprite;
+        }
+        else
+        {
+            _passwordField.contentType = InputField.ContentType.Password;
+            _buttonImage.sprite = _eyeClosedSprite;
+        }
+        _passwordField.ForceLabelUpdate();
+    }
+
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
