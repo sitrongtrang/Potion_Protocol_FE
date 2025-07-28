e90fc7efa24459b7acee24772995d573da016061
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-28 14:55:34 +0700

    add

--- Assets/Scripts/Network/Connection/NetworkMessages/Server/GameState/GameStateUpdate.cs
+++ Assets/Scripts/Network/Connection/NetworkMessages/Server/GameState/GameStateUpdate.cs
@@ -11,15 +11,27 @@ public class GameStatesUpdate : ServerMessage
 [Serializable]
 public class GameStateUpdate : IStateSnapshot, IServerStateSnapshot
 {
+    [FieldOrder(0)]
     public int ServerSequence;
+    [FieldOrder(1)]
     public int ProcessedInputSequence;
+    [FieldOrder(2)]
     public PlayerState[] PlayerStates;
+    [FieldOrder(3)]
     public StationState[] StationStates;
+    [FieldOrder(4)]
+    public OreState[] OreStates;
+    [FieldOrder(5)]
+    public PlantState[] PlantStates;
+    [FieldOrder(6)]
     public EnemyState[] EnemyStates;
+    [FieldOrder(7)]
     public ItemState[] ItemStates;
+    [FieldOrder(8)]
     public int CurrentGameTime;
-    public int CurrentScore;
+    //     public int CurrentScore;
 
     int IStateSnapshot.ProcessedInputSequence => ProcessedInputSequence;
     int IServerStateSnapshot.ServerSequence => ServerSequence;
 }
+
