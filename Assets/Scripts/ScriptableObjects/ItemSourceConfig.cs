5f909b6380faaf9ded1c38b353b1955dde740a24
sitrongtrang <sitrongtrang@gmail.com>
2025-07-31 14:02:29 +0700

    Update ItemSourceConfig.cs

--- Assets/Scripts/ScriptableObjects/ItemSourceConfig.cs
+++ Assets/Scripts/ScriptableObjects/ItemSourceConfig.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemSourceConfig", menuName = "Scriptable Objects/ItemSourceConfig")]
 public class ItemSourceConfig : EntityConfig
 {
-    [SerializeField] private string _id;
-    public string Id => _id;
-
     [SerializeField] private ItemConfig _droppedItem;
     public ItemConfig DroppedItem => _droppedItem;
 }
