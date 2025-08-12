bab5c8cabc4e25ad66a6a84508de33ac9c3dfc46
sitrongtrang <sitrongtrang@gmail.com>
2025-08-12 20:55:10 +0700

    change interpolators

--- Assets/Scripts/NetworkControllers/EnemyControllerNetwork.cs
+++ Assets/Scripts/NetworkControllers/EnemyControllerNetwork.cs
@@ -37,8 +37,8 @@ public class EnemyControllerNetwork : NetworkBehaviour
     {
         _interpolator.IncrementAndInterpolate((serverState) =>
         {
-            float xDir = Mathf.Abs(serverState.PositionX - transform.position.x);
-            float yDir = Mathf.Abs(serverState.PositionY - transform.position.y);
+            float xDir = serverState.PositionX - transform.position.x;
+            float yDir = serverState.PositionY - transform.position.y;
             Vector2 dir = new Vector2(xDir, yDir).normalized;
 
             _animator.SetBool("IsMoving", dir != Vector2.zero);
