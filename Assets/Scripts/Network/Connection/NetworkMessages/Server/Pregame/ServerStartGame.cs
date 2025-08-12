19fa7390967fab4106aee6c7c662db4e7c19c80e
Quan Nguyen <minhquan2nd@gmail.com>
2025-08-12 15:31:04 +0700

    change order

--- Assets/Scripts/Network/Connection/NetworkMessages/Server/Pregame/ServerStartGame.cs
+++ Assets/Scripts/Network/Connection/NetworkMessages/Server/Pregame/ServerStartGame.cs
@@ -10,11 +10,11 @@ public class ServerStartGame : ServerMessage
     [FieldOrder(2)]
     public string[] PlayerIds;
     [FieldOrder(3)]
-    public string AlchemyId;
-    [FieldOrder(4)]
-    public string[] FurnaceIds;
-    [FieldOrder(5)]
     public int Level;
+    [FieldOrder(4)]
+    public string AlchemyId;
+    [FieldOrder(5)]
+    public string[] FurnaceIds;
 
     public ServerStartGame() : base(NetworkMessageTypes.Server.Pregame.StartGame) { }
 }
