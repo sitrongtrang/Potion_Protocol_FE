3aa10b644a26ec36e8f1e1c821a3c93385dd9448
Quan Nguyen <minhquan2nd@gmail.com>
2025-08-12 09:49:30 +0700

    change some logics

--- Assets/Scripts/NetworkControllers/PlayerNetworkController.cs
+++ Assets/Scripts/NetworkControllers/PlayerNetworkController.cs
@@ -107,8 +107,8 @@ public class PlayerNetworkController : MonoBehaviour
         {
             _interpolator.IncrementAndInterpolate((serverState) =>
             {
-                float xDir = Mathf.Abs(serverState.PositionX - transform.position.x);
-                float yDir = Mathf.Abs(serverState.PositionY - transform.position.y);
+                float xDir = serverState.PositionX - transform.position.x;
+                float yDir = serverState.PositionY - transform.position.y;
                 Vector2 dir = new Vector2(xDir, yDir).normalized;
                 _playerDir = dir != Vector2.zero ? dir : _playerDir;
 
@@ -161,10 +161,14 @@ public class PlayerNetworkController : MonoBehaviour
     {
         PlayerInputSnapshot cpy = new(inputSnapshot);
 
-        if (!cpy.HasInput()) return;
+        if (!cpy.HasInput())
+        {
+            TriggerMoveAnimation(_playerDir, false);
+            return;
+        }
 
         TryMove(cpy);
-        
+
         // else
         // {
         //     TriggerMoveAnimation(_playerDir, false);
